63522e584d7e7ce05b04180610955883ce579cc3
guiemi <guiemi.teixeira@gmail.com>
2018-10-31 00:22:04 -0300

    Adiciona a primeira task no index.

--- MinimalList/core/admin.py
+++ MinimalList/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from core.models import Tasks
 
+admin.site.register(Tasks)
 # Register your models here.
--- MinimalList/core/models.py
+++ MinimalList/core/models.py
@@ -3,7 +3,12 @@ from django.utils import timezone
 
 
 class Tasks(models.Model):
+    class Meta:
+        verbose_name_plural = "Tasks"
+
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+    
