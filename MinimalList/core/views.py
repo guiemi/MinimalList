0cd11a2dbb0348f8c752053ebc62b6d99e7ae42e
guiemi <guiemi.teixeira@gmail.com>
2018-11-04 16:54:49 -0200

    Implementa o botão e o modal de inserção de nova task

--- MinimalList/core/views.py
+++ MinimalList/core/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Tasks
 from datetime import datetime
+
 
 def index(request):
     tasks = Tasks.objects.all()
     today_date = datetime.now()
 
-    return render(request, 'index.html', {
-        'tasks': tasks,
-        'today_date': today_date,
-    })
+    if request.method == 'POST':
+        if "taskAdd" in request.POST:
+            taskAdd = request.POST["taskAdd"]
+            Todo = Tasks(taskAdd=taskAdd)
+            Todo.save()
+            return redirect('/')
+    elif request.method == 'GET':
+        return render(request, 'index.html', {
+            'tasks': tasks,
+            'today_date': today_date,
+        })
+
+
+# def new_task(request):
+#     add_task = Tasks(task_name = request.POST['new_task'])
+#     add_task.save()
+#     return HttpResponseRedirect('/')
