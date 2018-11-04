ad78cddec788cbf927f13c0bae4deca9be2d365d
guiemi <guiemi.teixeira@gmail.com>
2018-11-03 21:25:20 -0300

    Melhora a importação das views pro urls.py

--- MinimalList/MinimalList/urls.py
+++ MinimalList/MinimalList/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core import views
+from core.views import index
 from django.conf.urls import url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', index, name='index'),
 ]
